797ca7ccb7842816d48a652f7f06c3aa81108337
lolimay <1404363070@qq.com>
2018-08-25 22:54:04 +0800

    first blood

---
+++ manage-output/src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+import printMe from './print'
+
+function component() {
+    const element = document.createElement('div')
+    const btn = document.createElement('button')
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    btn.innerHTML = 
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
