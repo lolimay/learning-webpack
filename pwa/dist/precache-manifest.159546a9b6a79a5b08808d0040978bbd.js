057c674a4e95654ed22f7483757f76a15670f387
lolimay <1404363070@qq.com>
2018-08-29 19:52:32 +0800

    complete pwa

---
+++ pwa/dist/precache-manifest.159546a9b6a79a5b08808d0040978bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db42d380690879f84b1f",
+    "url": "vendor.2ab363e107cf2c845f31.js"
+  },
+  {
+    "revision": "e1d43ade4ac829b68a0c",
+    "url": "runtime.6c791de0ca144560461c.js"
+  },
+  {
+    "revision": "aa896577a0e81056b593a2f7d105c702",
+    "url": "index.html"
+  },
+  {
+    "revision": "2554bab1ef6759fdbeaf3f33002e34c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "338cd9ea8293c12ced96",
+    "url": "app.32ce26f027795aaeea3f.js"
+  }
+];
