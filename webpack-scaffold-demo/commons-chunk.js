c2cec5407b765b337986388bb3ddb2976d4dd320
lolimay <1404363070@qq.com>
2018-08-29 22:40:29 +0800

    complete init

--- webpack-scaffold-demo/commons-chunk.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable quotes */
-module.exports = function createCommonsChunkPlugin(chunk) {
-	return (
-		"new webpack.optimize.CommonsChunkPlugin({name:" + "'" + chunk + "'" +
-		",filename:" + "'" + chunk + "-[hash].min.js'})"
-	);
-};
